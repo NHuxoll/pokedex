fd39e4ce1c8c4dfc9448af27923d0a5c82a72350
NHuxoll <NHuxoll>
2024-08-06 20:11:48 +0200

    we can now call for help and exit

--- main.go
+++ main.go
@@ -6,15 +6,52 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
+}
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
 	for {
 		fmt.Print("Pokedex >")
 		for scanner.Scan() {
-			fmt.Println(scanner.Text())
+			commandAsked := scanner.Text()
+			command := commands[commandAsked]
+			command.callback()
 			fmt.Print("Pokedex >")
 		}
 		if err := scanner.Err(); err != nil {
+
 			fmt.Fprintln(os.Stderr, "reading standard input:", err)
 		}
 	}
